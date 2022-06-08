5d957af3c3252229f1966f2ff910435b9371f24b
OllieSAE <1020925@student.sae.edu.au>
2022-06-08 16:37:19 +1000

    behaviour tree and reacting to various npcs
    food, predator and mate npcs cause main critter to change colour
    health and energy basic implementation where npcs cause changes in health/energy
    forced sleep at 0 energy

---
+++ Assets/Team members/Ollie/Scripts/Interfaces.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ollie
+{
+    public interface iNPC
+    {
+
+    }
+
+    public interface iFood
+    {
+        
+    }
+
+    public interface iPredator
+    {
+        
+    }
+
+    public interface iMate
+    {
+        
+    }
+}
