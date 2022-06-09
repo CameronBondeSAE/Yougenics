68335e6dd2dbbf58c3205ec2a540cb9bda2849a9
Test <test@test.com>
2022-06-09 12:46:29 +1000

    StatesMachineUpdate

--- Assets/Team members/Alex/Scripts/AnotherAI.cs
+++ Assets/Team members/Alex/Scripts/AnotherAI.cs
@@ -1,26 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Alex;
+
 
 namespace Alex
 {
     public class AnotherAI : MonoBehaviour
     {
+        public float movementSpeed;
+        public float lookingSpeed = 1f;
+        public Transform Target;
+        public Alex.Food myFoodTarget;
+        Energy myEnergy;
+        Rigidbody rb;
+
         // Start is called before the first frame update
         void Start()
         {
-            Alex.StateBase stateBase = new Alex.StateBase();
+            /*
+            StateBase stateBase = new StateBase();
 
             stateBase.EatingTest();
 
             Eating eating = new Eating();
             eating.EatingTest();
+
+            */
+
+            ChangeState(GetComponent<LookingForFood>());
+            
         }
 
-        // Update is called once per frame
-        void Update()
+        
+        public StateBase currentState;
+
+        private void Update()
         {
-
+            if (GetComponent<Energy>().energyAmount <= 10)
+            {
+                // Sleep
+                ChangeState(GetComponent<Sleeping>());
+            }
+            else if (GetComponent<Energy>().energyAmount < 50)
+            {
+                // Look for food
+                ChangeState(GetComponent<LookingForFood>());
+            }
         }
+
+        // This works for ANY STATE
+        public void ChangeState(StateBase newState)
+        {
+            // Check if the state is the same and DON'T swap
+            if (newState == currentState)
+            {
+                return;
+            }
+
+            // At first 'currentstate' will ALWAYS be null
+            if (currentState != null)
+            {
+                currentState.enabled = false;
+            }
+
+            newState.enabled = true;
+
+            // New state swap over to incoming state
+            currentState = newState;
+        }
+
+
+
+
     }
 }
