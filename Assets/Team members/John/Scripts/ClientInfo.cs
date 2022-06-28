ee4499c10faf4a2da09a696cca10192f595f5e83
JohnEngstrom <engstromj124@gmail.com>
2022-06-28 21:53:05 +1000

    Lobby Updates

--- Assets/Team members/John/Scripts/ClientInfo.cs
+++ Assets/Team members/John/Scripts/ClientInfo.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class ClientInfo : NetworkBehaviour
 {
 
-    public string name = "test ";
+    public string clientName = "Player";
 
     public override void OnNetworkSpawn()
     {
