72c2385e9ebffcef9c9d2877d0b6eb17586460b1
ScreamAimMaya <91040635+ScreamAimMaya@users.noreply.github.com>
2022-07-06 11:42:27 +1000

    Critters can almost fuck

--- Assets/Team members/Maya/Scripts/PlannerCritter/CritterSenses.cs
+++ Assets/Team members/Maya/Scripts/PlannerCritter/CritterSenses.cs
@@ -29,10 +29,10 @@ namespace Maya
             aWorldState.Set(CritterAI.isTired, myEnergy.energyAmount <= 25);
             aWorldState.Set(CritterAI.canSeeEnemy, false);
             aWorldState.Set(CritterAI.canSeeFood, vision.whereFoodIs.Count >= 0);
-            aWorldState.Set(CritterAI.canSeeMate, false);
+            aWorldState.Set(CritterAI.canSeeMate, vision.potentialMatesIveSeen.Count > 0);
             aWorldState.Set(CritterAI.isNearEnemy, false);
             aWorldState.Set(CritterAI.isNearFood, touch.isNearFood);
-            aWorldState.Set(CritterAI.isNearMate, false);
+            aWorldState.Set(CritterAI.isNearMate, touch.isNearMate && touch.myMatesHornyToo);
             aWorldState.Set(CritterAI.mateIsHorny, false);
             
         }
--- Assets/Team members/Maya/Scripts/PlannerCritter/Touch.cs
+++ Assets/Team members/Maya/Scripts/PlannerCritter/Touch.cs
@@ -11,6 +11,11 @@ namespace Maya
         public Food foodImTouching;
         public float timeToEat;
         public bool isNearFood;
+
+        public Horny mateImTouching;
+        public bool myMatesHornyToo;
+        public float timeToMate;
+        public bool isNearMate;
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Food"))
@@ -18,6 +23,18 @@ namespace Maya
                 foodImTouching = other.GetComponent<Food>();
                 timeToEat = foodImTouching.energyValue / 2;
                 isNearFood = true;
+            }
+            
+            if (other.CompareTag("MayasCritter"))
+            {
+                mateImTouching = other.GetComponent<Horny>();
+                if(mateImTouching.currentHorny >= 75)
+                {
+                    myMatesHornyToo = true;
+                }
+                timeToMate = mateImTouching.currentHorny / 3;
+                isNearMate = true;
+                
             }
         }
     }
