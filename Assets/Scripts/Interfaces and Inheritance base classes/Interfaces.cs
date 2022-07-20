5aff493be600f08c206ec698f569318ebcf58434
minhngo1207 <1023252@student.sae.edu.au>
2022-07-20 15:11:04 +1000

    Interact() interface

--- Assets/Scripts/Interfaces and Inheritance base classes/Interfaces.cs
+++ Assets/Scripts/Interfaces and Inheritance base classes/Interfaces.cs
@@ -1,3 +1,4 @@
+using Minh;
 using UnityEngine;
 
 public interface IVehicleControls
---
+++ Assets/Team members/DucMinhNgo/Button/Interactf.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Kev;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Minh
+{
+    public class Interactf : MonoBehaviour, IInteractable
+    {
+        public float hp = 90;
+        public float curhp = 90;
+        public event Action dealdamage;
+        public event Action healing; 
+        
+        // Start is called before the first frame update
+        void FixedUpdate()
+        {
+            Dealdamage();
+            Interact();
+        }
+
+        // Update is called once per frame
+
+
+        public void Dealdamage()
+        {
+           
+                hp = curhp = hp - 10;
+                GetComponent<Renderer>().material.color = Color.gray;
+                if (hp == 0)
+                {
+                    Destroy(gameObject);
+                }
+                dealdamage?.Invoke();
+        }
+
+        public void Interact()
+        {
+            hp = curhp = hp + 10;
+            if (hp >= 100)
+            {
+                GetComponent<Renderer>().material.color = Color.green;
+                curhp = 100;
+                Debug.Log("Full Hp");
+            }
+            healing?.Invoke();
+        }
+    }
+}
+
