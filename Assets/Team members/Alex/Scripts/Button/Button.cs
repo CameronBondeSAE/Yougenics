4da41caefa2b7b48c46711e3ccf416fa2e4bccef
Cam <c.bonde@sae.edu>
2022-07-27 11:28:38 +1000

    Refactored shop code

--- Assets/Team members/Alex/Scripts/Button/Button.cs
+++ Assets/Team members/Alex/Scripts/Button/Button.cs
@@ -1,9 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using Alex;
-using Anthill.Effects;
-using DG.Tweening;
-using UnityEditor;
 using UnityEngine;
 using System;
 using Unity.Netcode;
