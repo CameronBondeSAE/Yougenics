7d954367943230c3f799a56fc2ff0dd586ccbe55
Alex <1024087@student.sae.edu.au>
2022-07-28 10:45:40 +1000

    Shop Working

--- Assets/Team members/Alex/Scripts/ItemShop/ItemInfoUI.cs
+++ Assets/Team members/Alex/Scripts/ItemShop/ItemInfoUI.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -8,31 +9,61 @@ namespace Alex
 
     public class ItemInfoUI : MonoBehaviour
     {
-        public  GameObject itemToSpawn;
-        public ItemInfo itemInterface;
-        string name = "";
-        string description = "";
-        float cost = 0f;
+        public ShopSingleItem shopSingleItem;
+        string nameTxt;
+        string descriptionTxt;
+        float energyRequiredTxt;
+        float buildTimeTxt;
+        float timeRemaning;
+        float startTimeTxt;
+        
 
-        TMP_Text text;
+        TMP_Text textMeshPro;
 
-        // Start is called before the first frame update
         void Start()
         {
-            itemInterface = itemToSpawn.GetComponent<IItem>().GetInfo();
+            ResetTimer();
 
-            name = itemInterface.name;
-            description = itemInterface.description;
-            cost = itemInterface.energyRequired;
-            text = GetComponent<TMP_Text>();
+            textMeshPro = GetComponent<TMP_Text>();
 
-            text.text = "Name: " + name + "\r\n" + "Description: " + description + "\r\n"  + "Energy Cost: " + cost;
+            ResetText();
+
         }
 
         // Update is called once per frame
         void Update()
         {
-            
+            if (shopSingleItem.beingBuilt)
+            {
+                if (timeRemaning > 0)
+                {
+                    timeRemaning -= Time.deltaTime;
+                }
+                else
+                {
+                    timeRemaning = 0f;
+                }
+                textMeshPro.text = nameTxt + " build in progress." + "\n\r" + "Time Remaining: " + timeRemaning.ToString("#");
+            }
+            else
+            {
+                ResetTimer();
+                ResetText();
+            }
+        }
+
+        void ResetTimer()
+        {
+            nameTxt = shopSingleItem.itemInfo.name;
+            descriptionTxt = shopSingleItem.itemInfo.description;
+            energyRequiredTxt = shopSingleItem.itemInfo.energyRequired;
+            buildTimeTxt = shopSingleItem.itemInfo.buildTime;
+            timeRemaning = buildTimeTxt;
+        }
+
+        void ResetText()
+        {
+            textMeshPro.text = "Name: " + nameTxt + "\r\n" + "Description: " + descriptionTxt + "\r\n"  + "Energy Cost: " + energyRequiredTxt + "\n\r" + "Build Time: " + buildTimeTxt;
         }
     }
 }
--- Assets/Team members/Alex/Scripts/ItemShop/Shop1/ShopSingleItem.cs
+++ Assets/Team members/Alex/Scripts/ItemShop/Shop1/ShopSingleItem.cs
@@ -1,25 +1,48 @@
+using System.Collections;
 using Alex;
 using UnityEngine;
 
 public class ShopSingleItem : MonoBehaviour
 {
-	public  ItemInfo   itemInterface; // Unity doesn't show interfaces in inspector
+	public  ItemInfo   itemInfo; // Unity doesn't show interfaces in inspector
 	public  GameObject itemToSpawn;   // So we'll use normal GameObject (the trouble is you can drag ANYTHING now, we need to check for the interface)
 	public  Transform  spawningPoint;
 	public  Spawner    spawner;
 	public  Button     button;
-	
+	public bool beingBuilt;
+	public bool canSpawn = true;
+
 
 	void Start()
 	{
-
-		itemInterface = itemToSpawn.GetComponent<IItem>().GetInfo();
-		//itemHeight = itemInterface.height;
+		itemInfo = itemToSpawn.GetComponent<IItem>().GetInfo();
 		button.buttonPressedEvent += SpawnItem;
 	}
 
 	public void SpawnItem()
 	{
-		spawner.SpawnSingle(itemToSpawn, spawningPoint.position +  new Vector3(0, itemInterface.height,0), spawningPoint.rotation);
+		Wrapper();
+	}
+
+	public void Wrapper()
+	{
+		if (beingBuilt == false)
+		{
+			StartCoroutine(StartBuild());
+		}
+	}
+	
+
+	public IEnumerator StartBuild()
+	{
+		if (canSpawn)
+		{
+			canSpawn = false;
+			beingBuilt = true;
+			yield return new WaitForSeconds(itemInfo.buildTime);
+			spawner.SpawnSingle(itemToSpawn, spawningPoint.position +  new Vector3(0, itemInfo.height,0), spawningPoint.rotation);
+			canSpawn = true;
+			beingBuilt = false;
+		}
 	}
 }
