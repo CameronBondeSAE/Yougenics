d7e9dc21bb3ceca732fd6d60cb29d2565ed00b96
Luke Archbold <1024504@student.sae.edu.au>
2022-08-04 18:35:18 +1000

    Luke Biome Manager Update

--- Assets/Team members/Luke/Scripts/Grid Stuff/BiomeNode.cs
+++ Assets/Team members/Luke/Scripts/Grid Stuff/BiomeNode.cs
@@ -12,22 +12,19 @@ namespace Luke
 		public BiomeManager Bm;
 		
 		public BiomeGroundTypes groundType;
-		private float fertility;
-		private float temperature;
+		private float _fertility;
+		private float _temperature;
 
-		public Vector2Int iterators = new (0,0);
-		public float blueness = 0;
-		
 		#endregion
 		
 		#region Properties
 		
 		public float Fertility
 		{
-			get => fertility;
+			get => _fertility;
 			set
 			{
-				fertility = value;
+				_fertility = value;
 				CalculateTemperature();
 			}
 		}
@@ -46,48 +43,30 @@ namespace Luke
 			{
 				case BiomeGroundTypes.Alpine:
 				{
-					temperature = 25f - worldPosition.y + fertility*5f;
+					_temperature = 25f - worldPosition.y + _fertility*5f;
 					break;
 				}
 				case BiomeGroundTypes.Tundra:
 				{
-					temperature = 20f + fertility*5f;
+					_temperature = 20f + _fertility*5f;
 					break;
 				}
 				case BiomeGroundTypes.Field:
 				{
-					temperature = 25f + fertility * 5f;
+					_temperature = 25f + _fertility * 5f;
 					break;
 				}
 				case BiomeGroundTypes.Forest:
 				{
-					temperature = 25f + fertility * 15f;
+					_temperature = 25f + _fertility * 15f;
 					break;
 				}
 				case BiomeGroundTypes.Desert:
 				{
-					temperature = 45f - fertility * 15f;
+					_temperature = 45f - _fertility * 15f;
 					break;
 				}
 			}
-		}
-		
-		public void SpreadFertility()
-		{
-			/*if (neighbours[iterators.x, iterators.y] == null) return;
-			blueness = 1;
-			BiomeNode node = (BiomeNode) neighbours[iterators.x, iterators.y];
-			
-			if (fertility <= node.Fertility)
-			{
-				node.Fertility -= (1-fertility) * 0.1f;
-			}
-			else
-			{
-				node.Fertility += fertility * 0.12f;
-			}
-
-			blueness = 0;*/
 		}
 	}
 
