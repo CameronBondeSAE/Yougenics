254cc8b20684a6067d900f125897101df7202183
Luke Archbold <1024504@student.sae.edu.au>
2022-08-10 17:07:01 +1000

    Luke Update - Refactored Critter to use CreatureBase

---
+++ Assets/Team members/Luke/Scripts/LukeAntenna.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Luke
+{
+	public class LukeAntenna : MonoBehaviour
+	{
+		[SerializeField] private GameObject parent;
+		
+		// Start is called before the first frame update
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void FixedUpdate()
+		{
+			// Physics.Raycast()
+		}
+	}
+}
