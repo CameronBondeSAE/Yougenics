8670fae8dec356af86bb4bff789336fea4dcd685
Luke Archbold <1024504@student.sae.edu.au>
2022-08-11 22:54:40 +1000

    Luke Update
    Changed Turning to use RelativeTorque
    Made new shader for Beam
    Changed mass and force mode for critters.
    Removed use of AStar pathing when path is very short.

--- Assets/Team members/Luke/Scripts/LukeAntenna.cs
+++ Assets/Team members/Luke/Scripts/LukeAntenna.cs
@@ -6,18 +6,29 @@ namespace Luke
 {
 	public class LukeAntenna : MonoBehaviour
 	{
-		[SerializeField] private GameObject parent;
+		[SerializeField] private Rigidbody _rb;
+		[SerializeField] private Transform _transform;
+		public Vector3 direction;
+		public float acceleration;
+		public float turnSpeed;
 		
 		// Start is called before the first frame update
-		void Start()
+		void OnEnable()
 		{
-
+			_rb = GetComponent<Rigidbody>();
+			_transform = GetComponent<Transform>();
 		}
 
 		// Update is called once per frame
 		void FixedUpdate()
 		{
-			// Physics.Raycast()
+			Debug.DrawLine(_transform.position, _transform.TransformDirection(direction));
+			if (Physics.Linecast(_transform.position, _transform.TransformDirection(direction), out RaycastHit hitInfo))
+			{
+				float mag = Vector3.Magnitude(direction);
+				_rb.AddForce(-direction*(acceleration*(mag-hitInfo.distance)/mag));
+				_rb.AddRelativeTorque(new Vector3(0,Mathf.Sign(direction.x)*turnSpeed*((mag-hitInfo.distance)/mag),0));
+			}
 		}
 	}
 }
