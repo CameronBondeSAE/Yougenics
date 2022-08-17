7324c26950003627104403f5ace119c901b6ea32
minhngo1207 <1023252@student.sae.edu.au>
2022-08-17 17:26:05 +1000

    Minh's level added to John's level manager

--- Assets/Team members/DucMinhNgo/TerrainGenerator/Terrain3DGenerator.cs
+++ Assets/Team members/DucMinhNgo/TerrainGenerator/Terrain3DGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,49 +7,25 @@ public class Terrain3DGenerator : MonoBehaviour
 {
     public float terrain3D = 10f;
     public float extended = 0.2f;
-    public int blockNo = 2;
-    public GameObject cubeprefab;
     public float adjustNo = 0.1f;
-    public float noisevalue3D = 0.1f;
     public TerrainGenerator terrainGenerator;
+
     void Start()
     {
-        GetComponent<TerrainGenerator>().Generatorformin += TerrainGen;
-        terrainGenerator.calculateHeightCallback = Heightcal;
+        //GetComponent<TerrainGenerator>().Generatorformin += TerrainGen;
+        terrainGenerator.calculateHeightCallback = Heightterrain;
         terrainGenerator.GenerateTerrain();
-        TerrainGen();
     }
 
-    void TerrainGen()
+    float Heightterrain(int x, int z)
     {
-        for (int x = 1; x < blockNo; x++)
-        {
-            for (int y = 1; y < blockNo; y++)
-            {
-                for (int z = 1; y < blockNo; y++)
-                {
-                    float noiseValue = (float)NoiseS3D.Noise(50, 50);
-                    terrain3D = Mathf.PerlinNoise(x * extended , y * extended);
-                    
-                    if (noiseValue >= 0f)
-                    {
-                        Instantiate(gameObject, new Vector3(x, y, z), Quaternion.identity);
-                       
-                    }
-                    
-                    
-                }
-            }
-            
-        }
-    }
-    float Heightcal(int x, int z)
-    {
-        
+
         terrain3D = Mathf.PerlinNoise(x * extended, z * extended);
 
         return terrain3D * adjustNo;
 
     }
-    
 }
+
+
+
