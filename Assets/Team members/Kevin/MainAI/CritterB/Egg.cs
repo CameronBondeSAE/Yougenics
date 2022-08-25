94b3d91a73c03d7595c7537b8a1228e268ddc5b0
minhngo1207 <1023252@student.sae.edu.au>
2022-08-25 19:43:37 +1000

    Revert "Revert "d""
    This reverts commit f3c77523b7886ea1ebcf63c20f419138c517a951.

---
+++ Assets/Team members/Kevin/MainAI/CritterB/Egg.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Kevin
+{
+    public class Egg : MonoBehaviour
+    {
+        public float hatchTimer = 25f;
+        public List<GameObject> critterPrefabs;
+        private void OnEnable()
+        {
+            StartCoroutine(Birth());
+            StartCoroutine(Hatch());
+        }
+
+        IEnumerator Birth()
+        {
+            yield return new WaitForSeconds(hatchTimer - 0.5f);
+            Instantiate(critterPrefabs[Random.Range(0,3)], transform.position, Quaternion.identity);
+        }
+        
+        IEnumerator Hatch()
+        {
+            yield return new WaitForSeconds(hatchTimer);
+            Destroy(gameObject);
+        }
+    }
+}
+
